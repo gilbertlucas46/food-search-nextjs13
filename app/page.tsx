0f41bedc8ec541dbde802529d20785ae165d0783
Gilbert <lucas@gilbert.codes>
2024-01-27 15:17:10 +0800

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Image from "next/image";
+import styles from "@/styles/page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <main className={styles.main}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia magni
+        repellendus itaque minima ullam, quas quisquam exercitationem nesciunt
+        commodi, quis temporibus libero consequuntur, natus cupiditate incidunt
+        culpa in impedit odit.
+      </main>
+      ;
+    </>
+  );
+}
